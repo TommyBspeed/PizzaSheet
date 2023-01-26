763f67ddb88ac966bc198f9c9719a255e7c8b078
Tommy Boileau <tommybspeed@gmail.com>
2023-01-26 13:07:06 -0800

    initial setup with both bootstrap and tailwind

---
+++ pizza-sheet/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footerContainer">
+      <h3 className="footerText">Made with ❤️ by TommyBspeed</h3>
+    </footer>
+  );
+}
---
+++ pizza-sheet/src/components/PizzaList/PizzaList.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./PizzaList.css";
+
+export default function PizzaList() {
+  return <div>PizzaList</div>;
+}
