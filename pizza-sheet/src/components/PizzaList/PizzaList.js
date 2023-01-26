995f0ca29a3f83c016e2ec3c9c4a7aab4452ecc2
Tommy Boileau <tommybspeed@gmail.com>
2023-01-26 13:11:08 -0800

    readme update

--- pizza-sheet/src/components/PizzaList/PizzaList.js
+++ pizza-sheet/src/components/PizzaList/PizzaList.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./PizzaList.css";
+import ListGroup from "react-bootstrap/ListGroup";
 
 export default function PizzaList() {
-  return <div>PizzaList</div>;
+  return (
+    <ListGroup>
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+  );
 }
